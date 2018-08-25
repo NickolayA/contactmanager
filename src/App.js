578b28abc2534139264fe75f37600e6f5b84c01d
vipz3 <vipz3@mail.ru>
2018-08-25 20:35:03 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Contacts from "./components/contacts/Contacts";
+import Header from "./components/layout/Header";
+import AddContact from "./components/contacts/AddContact";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+import { Provider } from "./context";
+import Test from "./components/test/Test";
+import EditContact from "./components/contacts/EditContact";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contact manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/test" component={Test} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
